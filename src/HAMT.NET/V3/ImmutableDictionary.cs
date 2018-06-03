c11e7f02840604fdeb1e7474503befad0f212dbd
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2018-06-03 12:11:07 +0300

    V3 with dynamic struct layout

---
+++ src/HAMT.NET/V3/ImmutableDictionary.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace HAMT.NET.V3
+{
+    public abstract class ImmutableDictionary<TKey, TValue> where TKey : IEquatable<TKey>
+    {
+        public static readonly ImmutableDictionary<TKey, TValue> Empty = new EmptyNode<TKey, TValue>();
+
+        protected const int Shift = 6;
+        protected const int Mask = (1 << Shift) - 1;
+
+        public ImmutableDictionary<TKey, TValue> Add(TKey key, TValue value)
+        {
+            return Add(key, value, (uint) key.GetHashCode(), 0);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return ContainsKey(key, (uint) key.GetHashCode(), 0);
+        }
+
+        internal abstract ImmutableDictionary<TKey, TValue> Add(TKey key, TValue value, uint hash, int shift);
+        internal abstract bool ContainsKey(TKey key, uint hash, int shift);
+    }
+
+    internal sealed class EmptyNode<TKey, TValue> : ImmutableDictionary<TKey, TValue> where TKey : IEquatable<TKey>
+    {
+        internal override ImmutableDictionary<TKey, TValue> Add(TKey key, TValue value, uint hash, int shift) =>
+            new BitMapNode<TKey, TValue, ValueNodes<TKey, TValue>>(0, null, 1U << (int)((hash >> shift) & Mask), new ValueNodes<TKey, TValue>(key, value));
+
+        internal override bool ContainsKey(TKey key, uint hash, int shift) => false;
+    }
+
+    internal sealed class BitMapNode<TKey, TValue, TValues> : ImmutableDictionary<TKey, TValue> 
+        where TKey : IEquatable<TKey> where TValues: struct, IValueNodes<TKey, TValue>
+    {
+        private readonly ulong _bitmapNodes;
+        private readonly ulong _bitmapValues;
+        private readonly ImmutableDictionary<TKey, TValue>[] _nodes;
+        private readonly TValues _values;
+
+        public BitMapNode(ulong bitmapNodes, ImmutableDictionary<TKey, TValue>[] nodes, ulong bitmapValues, TValues values)
+        {
+            _bitmapNodes = bitmapNodes;
+            _nodes = nodes;
+            _bitmapValues = bitmapValues;
+            _values = values;
+        }
+
+        internal override ImmutableDictionary<TKey, TValue> Add(TKey key, TValue value, uint hash, int shift)
+        {
+            var bit = 1U << (int) ((hash >> shift) & Mask);
+            if ((_bitmapNodes & bit) != 0)
+            {
+                var newNodes = new ImmutableDictionary<TKey, TValue>[_nodes.Length];
+                Array.Copy(_nodes, newNodes, _nodes.Length);
+                var index = Popcnt.PopCount((_bitmapNodes >> (int) bit) & Mask);
+                newNodes[index] = _nodes[index].Add(key, value, hash, shift + Shift);
+                return new BitMapNode<TKey, TValue, TValues>(_bitmapNodes, newNodes, _bitmapValues, _values);
+            }
+            else if ((_bitmapValues & bit) != 0)
+            {
+                // TODO
+                throw new NotImplementedException();
+            }
+            else
+            {
+                var index = (uint)Popcnt.PopCount((_bitmapValues >> (int)bit) & Mask);
+                return _values.Add(key, value, _bitmapNodes, _nodes, _bitmapValues, index);
+            }
+        }
+
+        internal override bool ContainsKey(TKey key, uint hash, int shift)
+        {
+            var bit = 1U << (int) ((hash >> shift) & Mask);
+            if ((_bitmapNodes & bit) != 0)
+            {
+                var index = Popcnt.PopCount((_bitmapNodes >> (int)bit) & Mask);
+                return _nodes[index].ContainsKey(key, hash, shift + Shift);
+            }
+            else if ((_bitmapValues & bit) != 0)
+            {
+                var index = Popcnt.PopCount((_bitmapValues >> (int)bit) & Mask);
+                return _values.ContainsKey(key, hash, index);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/HAMT.NET/V3/ValueNodes.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace HAMT.NET.V3
+{
+    public struct ValueNodes<TKey, TValue> : IValueNodes<TKey, TValue> where TKey : IEquatable<TKey>
+    {
+        public ValueNodes(TKey key, TValue value)
+        {
+            _key = key;
+            _value = value;
+        }
+
+        private readonly TKey _key;
+        private readonly TValue _value;
+
+        public bool ContainsKey(TKey key, uint hash, long index)
+        {
+            return hash == (uint) _key.GetHashCode() && key.Equals(_key);
+        }
+
+        public ImmutableDictionary<TKey, TValue> Add(TKey key, TValue value, ulong bitmapNodes,
+            ImmutableDictionary<TKey, TValue>[] nodes, ulong bitmapValues, uint index)
+        {
+            if (index == 0)
+            {
+                var values = new ValueNode2<TKey, TValue>(key, value, _key, _value);
+                return new BitMapNode<TKey, TValue, ValueNode2<TKey, TValue>>(bitmapNodes, nodes, bitmapValues | index,
+                    values);
+            }
+            else
+            {
+                var values = new ValueNode2<TKey, TValue>(_key, _value, key, value);
+                return new BitMapNode<TKey, TValue, ValueNode2<TKey, TValue>>(bitmapNodes, nodes, bitmapValues | index,
+                    values);
+            }
+        }
+    }
+
+    public struct ValueNode2<TKey, TValue> : IValueNodes<TKey, TValue> where TKey : IEquatable<TKey>
+    {
+        private readonly TKey _key1;
+        private readonly TValue _value1;
+        private readonly TKey _key2;
+        private readonly TValue _value2;
+
+        public ValueNode2(TKey key1, TValue value1, TKey key2, TValue value2)
+        {
+            _key1 = key1;
+            _value1 = value1;
+            _key2 = key2;
+            _value2 = value2;
+        }
+
+
+        public bool ContainsKey(TKey key, uint hash, long index)
+        {
+            if (index == 0)
+            {
+                return hash == (uint) _key1.GetHashCode() && key.Equals(_key1);
+            }
+
+            return hash == (uint) _key2.GetHashCode() && key.Equals(_key2);
+        }
+
+        public ImmutableDictionary<TKey, TValue> Add(TKey key, TValue value, ulong bitmapNodes,
+            ImmutableDictionary<TKey, TValue>[] nodes, ulong bitmapValues, uint index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IValueNodes<TKey, TValue> where TKey : IEquatable<TKey>
+    {
+        [Pure]
+        bool ContainsKey(TKey key, uint hash, long index);
+
+        [Pure]
+        ImmutableDictionary<TKey, TValue> Add(TKey key, TValue value, ulong bitmapNodes,
+            ImmutableDictionary<TKey, TValue>[] nodes, ulong bitmapValues, uint index);
+    }
+}
